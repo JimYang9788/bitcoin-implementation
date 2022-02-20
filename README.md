bc03a33627411c4a3f88f411f2c13ee514b2fe7b
Jim Yang <yang9788@yahoo.com>
2022-02-19 20:37:08 -0500

    setting up main and root direcotry

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-### Sample Bitcoin Implementation
+### Sample implementation of Bitcoin Blockchain
+
+This is a sample implementation of bitcoin 
+
+
+### To run 
+This repo uses C++17. To run the repo do 
+```
+cd bitcoin/build
+cmake ..
+./bitcoin.tsk
+```
---
+++ bitcoin/build/CMakeFiles/bitcoin.tsk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitcoin.tsk.dir/bitcoin.m.cpp.o"
+  "CMakeFiles/bitcoin.tsk.dir/bitcoin.m.cpp.o.d"
+  "bitcoin.tsk"
+  "bitcoin.tsk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitcoin.tsk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
